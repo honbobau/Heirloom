7d88f864adb04ad1d75584ffb62874249f124520
honbobau <c.hon.ip@gmail.com>
2016-07-07 16:54:28 -0400

    merge with development

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/test', function (req, res) {
+  res.send(
+    '<h1>File Upload Test</h1>' +
+    '<form action="..." method="post" enctype="multipart/form-data">' +
+    '<input type="file" name="imageUpload">' +
+    '</form>'
+    );
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
