b396910436d8c5944b60a0ac0f0a975bf581dfc7
honbobau <c.hon.ip@gmail.com>
2016-07-13 17:23:35 -0400

    setup upload to aws s3

--- routes/config.js
+++ routes/config.js
@@ -2,3 +2,6 @@ module.exports = {
   'secret': 'thisisthesecret',
   'database': 'postgres://localhost/heirloom_data'
 }
+
+const AWS_ACCESS_KEY_ID = 'AKIAI3RY2WC24RH65JYQ';
+const AWS_SECRET_ACCESS_KEY = '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';
